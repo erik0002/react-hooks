2357848a00a731a34129427b55a17f9344cc3e11
Yeliazar <erik.ruzhakov@mail.ru>
2021-08-01 21:48:35 +0300

    create alerts components, add hooks useState, useContext, create Search

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+export const Card = () => (
+    <div className="card">
+        <div className="card-body">
+            <img src="" alt="" className="card-img-top"/>
+            <h5 className="card-title">React JS</h5>
+            <Link to={'/profile/'} className="btn btn-primary">Открыть</Link>
+        </div>
+    </div>
+)
