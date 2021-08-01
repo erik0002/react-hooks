a525a95c4fda6103193121c3a3d803381f831936
Yeliazar <erik.ruzhakov@mail.ru>
2021-08-01 20:13:22 +0300

    create application, create navbar, add routing

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Information</h1>
+                    <p className="lead">Version of Application <strong>1.0.0</strong></p>
+                </div>
+            </div>
+        </div>
+    )
+}
