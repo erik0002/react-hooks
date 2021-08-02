ec45cafcc29af02f3ad65d89123cfc12706cae17
Yeliazar <erik.ruzhakov@mail.ru>
2021-08-02 23:01:46 +0300

    create request, update search and cards component

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -8,6 +8,10 @@ import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../ty
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
 
+const withCreds = url => {
+    return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
+
 export const GithubState = ({children}) => {
 
     const initialState = {
@@ -23,7 +27,7 @@ export const GithubState = ({children}) => {
         setLoading()
 
         const response = await axios.get(
-            `https://api.github.com/search/users?q=${value}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+            withCreds(`https://api.github.com/search/users?q=${value}&`)
         )
 
         dispatch({
@@ -35,22 +39,26 @@ export const GithubState = ({children}) => {
     const getUser = async name => {
         setLoading()
 
-        // ...
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}?`)
+        )
 
         dispatch({
             type: GET_USER,
-            payload: []
+            payload: response.data
         })
     }
 
     const getRepos = async name => {
         setLoading()
 
-        // ...
+        const response = await axios.get(
+            withCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+        )
 
         dispatch({
             type: GET_REPOS,
-            payload: []
+            payload: response.data
         })
     }
 
@@ -70,7 +78,8 @@ export const GithubState = ({children}) => {
 
     return (
         <GithubContext.Provider value={{
-            setLoading, search, getUser, getRepos, clearUsers, state
+            setLoading, search, getUser, getRepos, clearUsers,
+            user, users, repos, loading
         }}>
             {children}
         </GithubContext.Provider>
